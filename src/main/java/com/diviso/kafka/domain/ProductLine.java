fef84c82fedb6024ad01e466767784a6532cbc99
vishnuviki <vishnu.p.v@lxisoft.com>
2018-03-21 16:37:35 +0530

    topics all to be set

---
+++ src/main/java/com/diviso/kafka/domain/ProductLine.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.diviso.kafka.domain;
+
+/**
+ * @author vishnu
+ *
+ */
+public class ProductLine {
+	private Long productId;
+	private String productName;
+	
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/diviso/kafka/domain/Sale.java
@@ -0,0 +1,34 @@
+/**
+
+ * 
+ */
+package com.diviso.kafka.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vishnu
+ *
+ */
+public class Sale {
+	private Long customerId;
+	private List<ProductLine> productLines = new ArrayList<ProductLine>();
+	
+	public Long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+	public List<ProductLine> getProductLines() {
+		return productLines;
+	}
+	public void setProductLines(List<ProductLine> productLines) {
+		this.productLines = productLines;
+	}
+	
+	
+	
+
+}
