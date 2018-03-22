bc24886b03af249ec098108fbb2fb73a7d393fea
Fahad ES <fahades105@gmail.com>
2018-03-22 15:21:31 +0530

    Payment Null Error Fixed

--- src/main/java/com/diviso/kafka/web/rest/PaymentResource.java
+++ src/main/java/com/diviso/kafka/web/rest/PaymentResource.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class PaymentResource {
 	private PaymentService paymentService;
 	
 	@PostMapping("/payment")
-	public boolean createPayment(Payment payment) {
+	public boolean createPayment(@RequestBody Payment payment) {
 		log.info("Payment creation : {} ", payment);
 		
 		return paymentService.save(payment);
