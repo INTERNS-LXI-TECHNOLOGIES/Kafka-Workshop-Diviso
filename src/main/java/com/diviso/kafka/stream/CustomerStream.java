abac4be1fd98b6f0bc930e3852f827827d70484e
Fahad ES <fahades105@gmail.com>
2018-03-22 12:45:02 +0530

    Customer Reg Updates

--- src/main/java/com/diviso/kafka/stream/CustomerStream.java
+++ src/main/java/com/diviso/kafka/stream/CustomerStream.java
@@ -1,18 +1,13 @@
 package com.diviso.kafka.stream;
 
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.SubscribableChannel;
 
 public interface CustomerStream {
 
 	static final String TOPIC2="registrationoutput";
-	static final String TOPIC1="registrationoutput";
 	
 	@Output(TOPIC2)
 	MessageChannel write();
 	
-	@Input(TOPIC1)
-	SubscribableChannel read();
 }
